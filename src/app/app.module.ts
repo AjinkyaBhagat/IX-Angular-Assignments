983b5c22ffd2fc6f4df5947fbe38f9f746febf2c
AjinkyaBhagat <81403374+AjinkyaBhagat@users.noreply.github.com>
2022-06-15 23:34:56 +0530

    added assignment 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { SidebarComponentComponent } from './sidebar-component/sidebar-component.component';
+import { ContentComponentComponent } from './content-component/content-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    SidebarComponentComponent,
+    ContentComponentComponent
   ],
   imports: [
     BrowserModule,
